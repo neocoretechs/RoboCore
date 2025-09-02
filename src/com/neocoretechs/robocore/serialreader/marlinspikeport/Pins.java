692ad3986d49a6d3641356d6c4d7b20d4768879a
NeoCoreTechs - New Core Technologies <groffj@neocoretechs.com>
2025-09-02 12:54:50 -0700

    Update Pins.java

--- src/com/neocoretechs/robocore/serialreader/marlinspikeport/Pins.java
+++ src/com/neocoretechs/robocore/serialreader/marlinspikeport/Pins.java
@@ -2,52 +2,32 @@ package com.neocoretechs.robocore.serialreader.marlinspikeport;
 
 import java.io.IOException;
 import java.util.Arrays;
-/*
-import com.pi4j.io.gpio.GpioController;
-import com.pi4j.io.gpio.GpioFactory;
-import com.pi4j.io.gpio.GpioPin;
-import com.pi4j.io.gpio.GpioPinAnalogInput;
-import com.pi4j.io.gpio.GpioPinDigitalInput;
-import com.pi4j.io.gpio.GpioPinDigitalOutput;
-import com.pi4j.io.gpio.OdroidC1Pin;
-import com.pi4j.io.gpio.OdroidGpioProvider;
-import com.pi4j.io.gpio.Pin;
-import com.pi4j.io.gpio.PinMode;
-import com.pi4j.io.gpio.PinState;
-import com.pi4j.platform.Platform;
-import com.pi4j.platform.PlatformAlreadyAssignedException;
-import com.pi4j.platform.PlatformManager;
-import com.pi4j.util.CommandArgumentParser;
-*/
+
 import com.neocoretechs.robocore.GpioNative;
 /**
- * If we are not using the default Raspberry Pi platform, we should
- * explicitly assign the platform as the Odroid platform.<p/>
- * Note that some pins, such as pin 7, will give a device not found or device busy runtime exception
- * when provisioning. These pins are, for whatever reason, inaccessible even with command line tools.<p/>
- * The 'pins' referred to here are the WiringPi GPIO pin designations which map to the physical 40 pin header as follows:<p/>
- * Phy | WPi <br/>
- * 11  | 0		<br/>
- * 12  | 1		<br/>
- * 13  | 2		<br/>
- * 15  | 3		<br/>
- * 16  | 4		<br/>
- * 18  | 5		<br/>
- * 22  | 6		<br/>
- * 7   | 7	(unusable Odroid)	<br/>
- * 24  | 10		<br/>
- * 26  | 11		<br/>
- * 19  | 12	(PWM1)	<br/>
- * 21  | 13		<br/>
- * 23  | 14		<br/>
- * 29  | 21		<br/>
- * 31  | 22		<br/>
- * 33  | 23	(PWM0)	<br/>
- * 35  | 24		<br/>
- * 36  | 27		<br/>
- * When designating pins, use the WPi numbers, except for the analog input pins which are designed as: <p>
- * 37  | AIN1	<br/>
- * 40  | AIN0	<br/>
+ * Conforms to new libgpiod protocols. {@link GpioNative} 
+ * explicitly assume the platform as the Odroid platform.<p>
+ * The 'pins' referred to here are the GPIO pin designations which map to the physical 40 pin header as follows:<p>
+ * Phy | <br>
+ * 1  | 1		<br>
+ * 2  | 2		<br>
+ * 3  | 3		<br>
+ * 4  | 4		<br>
+ * 5  | 5		<br>
+ * 6  | 6		<br>
+ * 7  | 7	(PWM_C pwmchip0/pwm0)	<br>
+ * 8  | 8		<br>
+ * 9  | 9		<br>
+ * 10 | 10		<br>
+ * 11 | 11		<br>
+ * 12  | 12	(PWM_E pwmchip4/pwm0)	<br>
+ * 13  | 13		<br>
+ * 14  | 14		<br>
+ * 15  | 15		<br>
+ * 16  | 16	(PIN_16 gpiochip1 offset 66)	<br>
+ * 17  | 17		<br>
+ * 18  | 18 (PIN_18 gpiochip1 offset 67)		<br>
+ * 19  | 19		<br>
  * @author Jonathan Groff Copyright (C) NeoCoreTechs 2022
  *
  */
@@ -57,14 +37,9 @@ public class Pins {
 	public static int pinsOut[] = new int[80];
 	public static int Pin[] = new int[80];
 	public static int err;
-	/*
-	private static GpioController gpioController = null;
-	static GpioPinDigitalInput[] pinsIn = new GpioPinDigitalInput[28];
-	static GpioPinDigitalOutput[] pinsOut = new GpioPinDigitalOutput[28];
-	static GpioPinAnalogInput[] apins = new GpioPinAnalogInput[2];
-	*/
+	
 	public static GpioNative gpio = new GpioNative();
-	public static String gpioChip = "gpiochip0";
+	public static String gpioChip = "gpiochip1";
 	public static boolean chipOpen = false;
 	static int analogPollRate = 100; // ms between analog change for firing
 	static int analogChangeThreshold = 511; // amount analog input has to change before firing
@@ -86,148 +61,6 @@ public class Pins {
     		throw new IOException("findChipLine error:"+pin);
     	Pin[pin] = 1;
     	return pin;
-		/*
-		if(gpioController == null) {
-			try {
-				PlatformManager.setPlatform(Platform.ODROID);
-			} catch (PlatformAlreadyAssignedException e) {
-				throw new RuntimeException(e);
-			}
-			gpioController = GpioFactory.getInstance();
-			// <OPTIONAL>
-	        // We can optionally override the default polling rate for the analog input monitoring (DEFAULT=50ms)
-	        // ... this is the rate at which the internal analog input monitoring thread will poll for changes
-	        OdroidGpioProvider.setAnalogInputPollingRate(analogPollRate); // milliseconds
-	        // <OPTIONAL>
-	        // We can optionally override the default listener value change threshold (DEFAULT=0)
-	        // ... this is the threshold delta value that the internal analog input monitoring thread must cross before
-	        //     dispatching a new analog input value change event
-	        // analog value must change in excess of 511 from the last event dispatched before dispatching a new change event
-	        OdroidGpioProvider.setAnalogInputListenerChangeThreshold(analogChangeThreshold);
-		}
-		//Pin[] allPins = OdroidC1Pin.allPins();
-		//for(Pin p: allPins)
-			//System.out.println("Pin "+p);
-		
-		Pin pipin = null;
-	    // ####################################################################
-        //
-        // IF YOU ARE USING AN ODROID C1/C1+ PLATFORM, THEN ...
-        //    When provisioning a pin, use the OdroidC1Pin class.
-        //
-        // ####################################################################
-		switch(pin) {
-        // lookup the pin by address
-			case 0:
-				pipin = CommandArgumentParser.getPin(
-		                OdroidC1Pin.class,    // pin provider class to obtain pin instance from
-		                OdroidC1Pin.GPIO_00);  // default pin if no pin argument found
-				break;
-			case 1:
-				pipin = CommandArgumentParser.getPin(
-						OdroidC1Pin.class,    // pin provider class to obtain pin instance from
-						OdroidC1Pin.GPIO_01);  // default pin if no pin argument found
-				break;
-			case 2:
-				pipin = CommandArgumentParser.getPin(
-		                OdroidC1Pin.class,    // pin provider class to obtain pin instance from
-		                OdroidC1Pin.GPIO_02);  // default pin if no pin argument found
-				break;
-			case 3:
-				pipin = CommandArgumentParser.getPin(
-		                OdroidC1Pin.class,    // pin provider class to obtain pin instance from
-		                OdroidC1Pin.GPIO_03);  // default pin if no pin argument found
-				break;
-			case 4:
-				pipin = CommandArgumentParser.getPin(
-		                OdroidC1Pin.class,    // pin provider class to obtain pin instance from
-		                OdroidC1Pin.GPIO_04);  // default pin if no pin argument found
-				break;
-			case 5:
-				pipin = CommandArgumentParser.getPin(
-		                OdroidC1Pin.class,    // pin provider class to obtain pin instance from
-		                OdroidC1Pin.GPIO_05);  // default pin if no pin argument found
-				break;
-			case 6:
-				pipin = CommandArgumentParser.getPin(
-		                OdroidC1Pin.class,    // pin provider class to obtain pin instance from
-		                OdroidC1Pin.GPIO_06);  // default pin if no pin argument found
-				break;
-			case 7:
-				pipin = CommandArgumentParser.getPin(
-		                OdroidC1Pin.class,    // pin provider class to obtain pin instance from
-		                OdroidC1Pin.GPIO_07);  // default pin if no pin argument found
-				break;
-			case 10:
-				pipin = CommandArgumentParser.getPin(
-		                OdroidC1Pin.class,    // pin provider class to obtain pin instance from
-		                OdroidC1Pin.GPIO_10);  // default pin if no pin argument found
-				break;
-			case 11:
-				pipin = CommandArgumentParser.getPin(
-		                OdroidC1Pin.class,    // pin provider class to obtain pin instance from
-		                OdroidC1Pin.GPIO_11);  // default pin if no pin argument found
-				break;
-			case 12:
-				pipin = CommandArgumentParser.getPin(
-		                OdroidC1Pin.class,    // pin provider class to obtain pin instance from
-		                OdroidC1Pin.GPIO_12);  // default pin if no pin argument found
-				break;
-			case 13:
-				pipin = CommandArgumentParser.getPin(
-		                OdroidC1Pin.class,    // pin provider class to obtain pin instance from
-		                OdroidC1Pin.GPIO_13);  // default pin if no pin argument found
-				break;
-			case 14:
-				pipin = CommandArgumentParser.getPin(
-		                OdroidC1Pin.class,    // pin provider class to obtain pin instance from
-		                OdroidC1Pin.GPIO_14);  // default pin if no pin argument found
-				break;
-			case 21:
-				pipin = CommandArgumentParser.getPin(
-		                OdroidC1Pin.class,    // pin provider class to obtain pin instance from
-		                OdroidC1Pin.GPIO_21);  // default pin if no pin argument found
-				break;
-			case 22:
-				pipin = CommandArgumentParser.getPin(
-		                OdroidC1Pin.class,    // pin provider class to obtain pin instance from
-		                OdroidC1Pin.GPIO_22);  // default pin if no pin argument found
-				break;
-			case 23:
-				pipin = CommandArgumentParser.getPin(
-		                OdroidC1Pin.class,    // pin provider class to obtain pin instance from
-		                OdroidC1Pin.GPIO_23);  // default pin if no pin argument found
-				break;
-			case 24:
-				pipin = CommandArgumentParser.getPin(
-		                OdroidC1Pin.class,    // pin provider class to obtain pin instance from
-		                OdroidC1Pin.GPIO_24);  // default pin if no pin argument found
-				break;
-			case 26:
-				pipin = CommandArgumentParser.getPin(
-		                OdroidC1Pin.class,    // pin provider class to obtain pin instance from
-		                OdroidC1Pin.GPIO_26);  // default pin if no pin argument found
-				break;
-			case 27:
-				pipin = CommandArgumentParser.getPin(
-		                OdroidC1Pin.class,    // pin provider class to obtain pin instance from
-		                OdroidC1Pin.GPIO_27);  // default pin if no pin argument found
-				break;
-			case 37:
-				pipin = CommandArgumentParser.getPin(
-		                OdroidC1Pin.class,    // pin provider class to obtain pin instance from
-		                OdroidC1Pin.AIN1);  // default pin if no pin argument found
-				break;
-			case 40:
-				pipin = CommandArgumentParser.getPin(
-		                OdroidC1Pin.class,    // pin provider class to obtain pin instance from
-		                OdroidC1Pin.AIN0);  // default pin if no pin argument found
-				break;
-			default:
-				throw new RuntimeException("Digital Pins limited to numbers 0,1,2,3,4,5,6,7 10,11,12,13,14 21,22,23,24 26,27 and analog in 37,40 (Pins 19 and 33 are PWM and cannot be assigned to digital)");
-		}
-		return pipin;
-		*/
 	}
 	/**
 	 * Assign pin as output
@@ -235,11 +68,8 @@ public class Pins {
 	 * @return
 	 * @throws IOException 
 	 */
-	public static void assignPin(int pin) throws IOException {
-		//Pin xpin = getPin(pin);
-		//GpioPinDigitalOutput opin = gpioController.provisionDigitalOutputPin(xpin,String.valueOf(pin),PinState.LOW);
-		//opin.setMode(PinMode.DIGITAL_OUTPUT);
-		//opin.setShutdownOptions(false, PinState.LOW); // false = unexport
+	public static void assignPin(int spin) throws IOException {
+		int pin = getPin("PIN_"+spin);
 		if((err = gpio.lineRequestOutput(pin)) < 0)
 	 		throw new IOException("lineRequestOutput error:"+err);
 		pinsOut[pin] = 1;
@@ -248,78 +78,30 @@ public class Pins {
 	}
 	
 	public static void unassignPin(int pin) throws IOException {
-		//pin.removeAllListeners();
-		//gpioController.unprovisionPin(pin);
-	  	if((err = gpio.lineRelease(pin)) < 0)
+	  	if(pinsOut[pin] == 1 && (err = gpio.lineRelease(pin)) < 0)
 	  		throw new IOException("unassignPin error:"+err);
 	  	Pin[pin] = 0;
 	  	pinsIn[pin] = 0;
 	  	pinsOut[pin] = 0;
 	}
 	
-	public static void assignInputPin(int pin) throws IOException {
-		//Pin xpin = getPin(pin);
-		//GpioPinDigitalInput ipin = gpioController.provisionDigitalInputPin(xpin,String.valueOf(pin));
-		//ipin.setMode(PinMode.DIGITAL_INPUT);
+	public static void assignInputPin(int spin) throws IOException {
+		int pin = getPin("PIN_"+spin);
 		if((err = gpio.lineRequestInput(pin)) < 0)
 	 		throw new IOException("lineRequestOutput error:"+err);
-		Pin[pin] = 1;
 		pinsIn[pin] = pin;
 		if(DEBUG)
 			System.out.printf("Pins.assignInputPin input pin set %d%n", pin);
-		//return ipin;
 	}
 	
-	public static /*GpioPinAnalogInput*/ void assignAnalogInputPin(int pin) throws IOException {
-		 if((err = gpio.lineRequestRisingEdgeEvents(pin)) < 0)
+	public static void assignAnalogInputPin(int spin) throws IOException {
+		int pin = getPin("PIN_"+spin);
+		if((err = gpio.lineRequestRisingEdgeEvents(pin)) < 0)
 			 throw new IOException("lineRequestRisingEdgeEvents error:"+err);
-			Pin[pin] = 1;
-			pinsIn[pin] = pin;
-		/*
-			Pin pipin = null;
-			GpioPinAnalogInput apin;
-			if(gpioController == null) {
-				try {
-					PlatformManager.setPlatform(Platform.ODROID);
-				} catch (PlatformAlreadyAssignedException e) {
-					throw new RuntimeException(e);
-				}
-				gpioController = GpioFactory.getInstance();
-				// <OPTIONAL>
-		        // We can optionally override the default polling rate for the analog input monitoring (DEFAULT=50ms)
-		        // ... this is the rate at which the internal analog input monitoring thread will poll for changes
-		        OdroidGpioProvider.setAnalogInputPollingRate(analogPollRate); // milliseconds
-		        // <OPTIONAL>
-		        // We can optionally override the default listener value change threshold (DEFAULT=0)
-		        // ... this is the threshold delta value that the internal analog input monitoring thread must cross before
-		        //     dispatching a new analog input value change event
-		        // analog value must change in excess of 511 from the last event dispatched before dispatching a new change event
-		        OdroidGpioProvider.setAnalogInputListenerChangeThreshold(analogChangeThreshold);
-			}
-			switch(pin) {
-				case 37:
-					pipin = CommandArgumentParser.getPin(
-			                OdroidC1Pin.class,    // pin provider class to obtain pin instance from
-			                OdroidC1Pin.AIN1);  // default pin if no pin argument found
-					apin = gpioController.provisionAnalogInputPin(pipin, String.valueOf(pin));
-					break;
-				case 40:
-					pipin = CommandArgumentParser.getPin(
-			                OdroidC1Pin.class,    // pin provider class to obtain pin instance from
-			                OdroidC1Pin.AIN0);  // default pin if no pin argument found
-					apin =  gpioController.provisionAnalogInputPin(pipin, String.valueOf(pin));
-					break;
-				default:
-					throw new RuntimeException("Analog pin values limited to 37, 40 for AIN1, AIN0, but got pin:"+pin);
-			}
-			//apin.setMode(PinMode.ANALOG_INPUT);
-			if(DEBUG)
-				System.out.printf("Pins.assignAnalogInputPin input pin set %d to %s%n", pin, apin);
-			return apin;
-			*/
+		pinsIn[pin] = pin;
 	}
 	
-	public static /*GpioPinDigitalInput*/ int getInputPin(int pin) throws IOException {
+	public static int getInputPin(int pin) throws IOException {
 		//return pinsIn[pin];
 		int ret = gpio.lineGetValue(pin);
 		if(ret < 0)
@@ -327,7 +109,7 @@ public class Pins {
 		return ret;
 	}
 	
-	public static /*GpioPinDigitalOutput*/ void getOutputPin(int pin, int val) throws IOException{
+	public static void getOutputPin(int pin, int val) throws IOException{
 		//return pinsOut[pin];
 		int ret = gpio.lineSetValue(pin, val);
 		if(ret < 0)
@@ -339,7 +121,7 @@ public class Pins {
 	 * @return 1 rising edge, 3 falling edge, 0 unknown
 	 * @throws IOException
 	 */
-	public static /*GpioPinAnalogInput*/ int getAnalogInputPin(int pin) throws IOException {
+	public static int getAnalogInputPin(int pin) throws IOException {
 		while((err = gpio.lineEventWait(pin)) == 0) {		
 		}
 		if(err < 0)
